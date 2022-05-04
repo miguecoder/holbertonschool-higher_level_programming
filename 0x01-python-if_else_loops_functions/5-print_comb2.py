e0d3883217303e739a0a2b117302e1c7a157c449
miguecoder <mmarqzm@gmail.com>
2022-05-03 22:15:22 -0500

    combination posibles

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for j in range(0, 99):
-    print(f"{j:02d}, ", end='')
-print(99)
+for j in range(0, 89):
+    if j / 10 < j % 10:
+        print(f"{j:02d}, ", end='')
+print(89)
