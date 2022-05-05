39b8bd49075cd71d23bdbd13755ea0ef8f4c16f4
miguecoder <mmarqzm@gmail.com>
2022-05-04 20:31:37 -0500

    remove a char wearing the positions of string

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     if n >= 0:
-        print(f"{str[:n] + str[n+1:]}")
+        copy = str[:n] + str[n+1:]
     else:
-        print(str)
+        copy = str
+    return copy
