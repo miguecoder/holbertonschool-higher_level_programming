a88b09d4e094fff66922b60ceb5b67d54c5fc0f9
miguecoder <mmarqzm@gmail.com>
2022-05-23 20:57:48 -0500

    Function that print a integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import sys
+from sys import stderr
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (TypeError, ValueError) as err:
-        print("Exception: {}".format(err), file=sys.stderr)
+        stderr.write('Exception: {}\n'.format(err))
         return False
     return True
