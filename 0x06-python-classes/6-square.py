f2305d5ff177cde9f06807073d264a07d35dcf4b
miguecoder <mmarqzm@gmail.com>
2022-05-24 22:01:16 -0500

    New private instance attribute: position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -50,7 +50,8 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print("")
+            return
         for i in range(self.__position[1]):
             print("")
         for j in range(self.__size):
-            print(f"{'_' * self.position[0]}{'#' * self.__size}")
+            print(f"{' ' * self.__position[0]}{'#' * self.__size}")
