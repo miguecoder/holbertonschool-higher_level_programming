8add8d10cbd2b46cc54c73b91d3a4071f46f94dd
miguecoder <mmarqzm@gmail.com>
2022-05-24 21:17:29 -0500

    Class Square with private instance attribute and instantiation opscional and public instance method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module define de class:Square with private instance attribute"""
+
+
+class Square:
+    """This method define a private instance attribute -> size"""
+    def __init__(self, size=0):
+        """Define like Private instance attribute: size"""
+        self.__size = size
+        if isinstance(size, int) is False:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+    """area is a new Public instance method"""
+    def area(self):
+        return self.__size * self.__size
