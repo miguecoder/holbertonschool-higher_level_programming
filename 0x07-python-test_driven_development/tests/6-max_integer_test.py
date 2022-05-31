996ae7257a6ffa359f56234add01f0fb2d5e20a2
miguecoder <mmarqzm@gmail.com>
2022-05-30 23:57:21 -0500

    Pycodestyle for 6-max_integer_test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,6 +6,7 @@ import unittest
 
 max_integer = __import__('6-max_integer').max_integer
 
+
 class InteractiveTestsMaxInteger(unittest.TestCase):
     """
     This class is a interactive test for the function
@@ -17,7 +18,7 @@ class InteractiveTestsMaxInteger(unittest.TestCase):
         """
         self.assertAlmostEquals(max_integer([10, 20, 30, 50]), 50)
         self.assertAlmostEquals(max_integer([50, 30, 10, 20]), 50)
-        self.assertAlmostEquals(max_integer([30, 2 ,34, 5]), 34)
+        self.assertAlmostEquals(max_integer([30, 2, 34, 5]), 34)
         self.assertAlmostEquals(max_integer([-1, -2, -3, -5]), -1)
         self.assertAlmostEquals(max_integer([1.2, 2.2, 3.3, 5.5]), 5.5)
         self.assertAlmostEquals(max_integer([50]), 50)
@@ -30,19 +31,19 @@ class InteractiveTestsMaxInteger(unittest.TestCase):
 
     def test_errors(self):
         """
-        Test for 
+        Test for
         """
         lista = [4, 4.5, "Hello", 10]
         with self.assertRaises(TypeError):
             max_integer(lista)
+
     def test_None(self):
         """
         Test for compare result with None like argument
         """
         with self.assertRaises(TypeError):
             max_integer(None)
-        
-        
+
 
 if __name__ == '__main__':
     unittest.main()
