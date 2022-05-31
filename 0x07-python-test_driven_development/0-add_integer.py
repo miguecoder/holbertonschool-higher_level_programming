61e5ec30fe4149ac0dc701d29fe5473eebf7985a
miguecoder <mmarqzm@gmail.com>
2022-05-30 23:52:53 -0500

    Add commit to function

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,6 +3,10 @@
 
 
 def add_integer(a, b=98):
+    """
+    function that adds 2 integers.
+    Returns an integer: the addition of a and b
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError('a must be an integer')
     if not isinstance(b, int) and not isinstance(b, float):
