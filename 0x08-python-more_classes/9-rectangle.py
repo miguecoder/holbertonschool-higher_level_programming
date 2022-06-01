0348a1d26eaa3bb5c2f7684c4db9c4dc472983be
miguecoder <mmarqzm@gmail.com>
2022-05-31 23:02:03 -0500

    Class method that returns a new Rectangle instance with width == height == size

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -91,7 +91,7 @@ class Rectangle:
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_2.area() >= rect_1.area():
-            return rect_1
+            return rect_2
         return rect_1
 
     @classmethod
