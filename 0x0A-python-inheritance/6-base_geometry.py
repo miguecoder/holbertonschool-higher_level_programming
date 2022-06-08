6daf16a27d43958aa44e88e90d0fcd5255bdf8a1
miguecoder <mmarqzm@gmail.com>
2022-06-07 23:55:14 -0500

    Public instance method: def area(self)

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''This module have the Class MyList'''
+
+
+class BaseGeometry():
+    '''A class empty'''
+    def area(self):
+        raise Exception('area() is not implemented')
