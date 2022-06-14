cfb906d1a93e32729789da151424140c21141057
miguecoder <mmarqzm@gmail.com>
2022-06-14 14:25:33 -0500

    Class rectangle that inherits of Base callas

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,6 +10,7 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Class Constructor"""
+
         self.__width = width
         self.__height = height
         self.__x = x
