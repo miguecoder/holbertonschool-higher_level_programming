2d28a934b18c7fb98593dcdfbcd85332e25b4831
miguecoder <mmarqzm@gmail.com>
2022-06-14 17:45:43 -0500

    Class Rectangle update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,10 +11,10 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """Class Constructor"""
 
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
@@ -22,53 +22,53 @@ class Rectangle(Base):
         """Getter function for width"""
         return self.__width
 
-    @width.setter
-    def width(self, value):
-        """Setter function for width"""
-        if type(value) is not int:
-            raise TypeError('width must be an integer')
-        if value <= 0:
-            raise ValueError('width must be > 0')
-        self.__width = value
-
     @property
-    def height(self):
+    def height(self): 
         """Getter function for height"""
         return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Setter function for height"""
-        if type(value) is not int:
-            raise TypeError('height must be an integer')
-        if value <= 0:
-            raise ValueError('height must be > 0')
-        self.__height = value
 
     @property
     def x(self):
         """getter function for x"""
         return self.__x
 
-    @x.setter
-    def x(self, value):
-        """Setter function fot x"""
-        if type(value) is not int:
-            raise TypeError('x must be an integer')
-        if value < 0:
-            raise ValueError('x must be >= 0')
-        self.__x = value
-
     @property
     def y(self):
         """getter function for y"""
         return self.__y
 
+    @width.setter
+    def width(self, value):
+        """Setter function for width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """Setter function for height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """Setter function fot x"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
     @y.setter
     def y(self, value):
         """Setter function for y"""
         if type(value) is not int:
-            raise TypeError('y must be an integer')
+            raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError('y must be >= 0')
+            raise ValueError("y must be >= 0")
         self.__y = value
