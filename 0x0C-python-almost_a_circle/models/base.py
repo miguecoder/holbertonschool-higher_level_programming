a7ad5a7e7fe35963bdc6f85fbf02a720c354eba3
miguecoder <mmarqzm@gmail.com>
2022-06-14 12:12:22 -0500

    Create files

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Thi module contain the class Base"""
+
+
+class Base:
+    """Class Base with private attribute and
+    public instance"""
+    __nb_objects = 0
+
+
+    def __init__(self, id=None):
+        """Constructor method"""
+        if id is not None:
+            self.id = id
+        else:
+            self.__nb_objects += 1
