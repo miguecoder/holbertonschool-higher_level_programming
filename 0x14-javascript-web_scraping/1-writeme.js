96fbe65d8d953e94b6e12ef973cd3e3882820484
miguecoder <miguel@Lap.localdomain>
2022-08-18 22:44:53 -0500

    script that writes a string to a file.

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const fs = require('fs')
-let data = process.argv[3];
+const fs = require('fs');
+const data = process.argv[3];
 fs.writeFile(process.argv[2], data, 'utf-8', (err) => {
   if (err) throw err;
-})
+});
