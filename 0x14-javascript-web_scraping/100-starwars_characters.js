be42c4ec34c18295190f3fedc652de4cc1ffb0d9
miguecoder <miguel@Lap.localdomain>
2022-08-18 23:24:58 -0500

    cript that prints all characters of a Star Wars movie:

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,19 +1,14 @@
 #!/usr/bin/node
-const request = require('request');
-const URL = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request.get(URL, (error, response, body) => {
-  if (error) {
-    console.error('error:', error);
-  }
-  const data = JSON.parse(body);
-  data.characters.forEach((char) => {
-    request.get(char, (error1, response1, body1) => {
-      if (error1) {
-        console.error('error:', error1);
-      }
-      const OneCharacter = JSON.parse(body1);
-      console.log(OneCharacter.name);
-    });
-    return 0;
+
+const axios = require('axios').default;
+
+axios.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2])
+  .then(function (response) {
+    // Success execution
+    for (const key of response.data.characters) {
+      axios.get(key)
+        .then((res) => {
+          console.log(res.data.name);
+        });
+    }
   });
-});
