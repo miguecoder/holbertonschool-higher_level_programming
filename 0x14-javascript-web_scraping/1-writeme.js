c3080e7d1e2f73ef13e5e0d801fde32cea585f1e
miguecoder <miguel@Lap.localdomain>
2022-08-18 22:45:35 -0500

    script that writes a string to a file.

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,3 +4,6 @@ const data = process.argv[3];
 fs.writeFile(process.argv[2], data, 'utf-8', (err) => {
   if (err) throw err;
 });
+/*
+https://www.geeksforgeeks.org/javascript-program-to-write-data-in-a-text-file/
+*/
