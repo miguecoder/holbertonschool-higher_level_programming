ad7e9cdcffbe8fd8b302d0b1f3f3739c774f2ad8
miguecoder <mmarqzm@gmail.com>
2022-09-04 20:30:15 -0500

    script that prints the first State object from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+script that prints the first State object from the database hbtn_0e_6_usa
+"""
+if __name__ == "__main__":
+    """
+    Creating connection to mysql
+    """
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            argv[1],
+            argv[2],
+            argv[3]
+        )
+    )
+
+    """
+    Setting up and starting session
+    """
+    Session = sessionmaker(engine)
+    session = Session()
+
+    """
+    Save in a temp varieble that is a
+    instance
+    """
+    temp = session.query(State).order_by(State.id).first()
+
+    """
+    If the table states is empty, print Nothing
+    followed by a new line
+    """
+    if temp is None:
+        print("Nothing")
+    else:
+        print(f"{temp.id}: {temp.name}")
