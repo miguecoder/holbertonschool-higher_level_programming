3b8ef23a6e0bb029efd4fe15039641e2f8239df7
miguecoder <mmarqzm@gmail.com>
2022-09-05 00:08:19 -0500

    Python script that fetches https://intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Python script non-importable
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    """
+    Python script that fetches https://intranet.hbtn.io/status
+    """
+    req = urllib.request.Request("https://intranet.hbtn.io/status")
+
+    with urllib.request.urlopen(req) as res:
+        html = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
