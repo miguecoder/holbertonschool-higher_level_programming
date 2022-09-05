7092c1b7b775362d398af9484a9ca208ba31ca5d
miguecoder <mmarqzm@gmail.com>
2022-09-05 13:31:53 -0500

    Python script that takes 2 arguments and print the last 10 commits of a repo

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 """
 Python script that takes 2 arguments in order to
-solve this challenge.
+solve this challenge:
+Please list 10 commits (from the most recent to oldest)
+of the repository “rails” by the user “rails”
+You must use the GitHub API, here is the documentation
+https://developer.github.com/v3/repos/commits/
+Print all commits by: `<sha>: <author name>` (one by line).
 """
 
 from sys import argv
 import requests
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    repo = argv[1]
+    username = argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(username, repo)
     response = requests.get(url)
     for commit in response.json()[:10]:
         print(
