82d03e05ea3eb6f4b4ed8eac5e66fa916cf5152a
miguecoder <mmarqzm@gmail.com>
2022-09-04 21:06:15 -0500

    script that adds the State object “Louisiana” to the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -33,5 +33,5 @@ if __name__ == "__main__":
     new_state = State(name="Louisiana")
     session.add(new_state)
     session.commit()
-    print(new_state)
+    print(new_state.id)
     session.close()
