9be0d1eddcb9fe37caaa51bbc2ab16f21268a150
miguecoder <mmarqzm@gmail.com>
2022-09-05 09:29:16 -0500

    Python script that fetches https://intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,9 +4,9 @@ Python script that fetches https://intranet.hbtn.io/status
 """
 
 
-import requests as req
+import requests
 
 if __name__ == "__main__":
-    body = req.get("https://intranet.hbtn.io/status")
+    body = requests.get("https://intranet.hbtn.io/status")
     print("Body response:\n\t- type: {}\n\t- content:\
          {}".format(type(body.text), body.text))
