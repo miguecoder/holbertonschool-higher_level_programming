e2858fc3fc1ee4620061e82e545edeb0e5aaecbb
miguecoder <mmarqzm@gmail.com>
2022-09-04 21:22:33 -0500

    script that prints all City objects from the database hbtn_0e_14_usa:

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+script that prints all City objects from the database hbtn_0e_14_usa:
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    Creating connection to mysql
+    """
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            argv[1],
+            argv[2],
+            argv[3]
+        )
+    )
+
+    """
+    Setting up and starting session
+    """
+    Session = sessionmaker(engine)
+    session = Session()
+    """
+    cicle that print instance City and State
+    to state name based on state_id, id and name
+    """
+    instance = session.query(City, State).join(State)
+    for inst_c, inst_s in inst_q.all():
+        print(f"{inst_s.name}: ({inst_c.id}) {inst_c.name}")
+
+    session.commit()
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Definition of class City
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    Create attributes id, name and state_id
+    like Columns with the underlying
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
